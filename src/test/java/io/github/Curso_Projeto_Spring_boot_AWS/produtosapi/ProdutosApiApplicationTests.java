97529640affad0c8b27afca558c87812dd1489b0
Kae Lucas <dev.kaeteodoro@gmail.com>
2025-12-03 20:32:13 -0300

    Adding the project

---
+++ src/test/java/io/github/Curso_Projeto_Spring_boot_AWS/produtosapi/ProdutosApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.Curso_Projeto_Spring_boot_AWS.produtosapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProdutosApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
